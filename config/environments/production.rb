d3ed51ac1cf53853d0f2f2f4b0ded85eeea85591
seantheplug <syliu1998@gmail.com>
2020-06-22 13:53:50 +0800

    add websocket and redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,12 +41,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://eskyconcierge.herokuapp.com', 'http://eskyconcierge.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -93,4 +94,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #set the cable server's URI for production.
+  config.web_socket_server_url = "wss://eskyconcierge.herokuapp.com/cable" 
 end
